d17e6ca87d21faef8aa0d3cff84bb50fa9bda483
Matheus Pereira dos Santos <msantos.2015@alunos.utfpr.edu.br>
2022-08-08 09:24:19 -0300

    Update identifier.c

--- intro-tecnicas-teste/identifier.c
+++ intro-tecnicas-teste/identifier.c
@@ -1,15 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int validateIdentifier(char* s);
+int valid_s(char ch);
+int valid_f(char ch);
+
 int validateIdentifier(char* s) {
    char achar;
    int i, valid_id = FALSE;
-
    if (strlen(s) > 0) {
       achar = s[0];
       valid_id = valid_s(achar);
-
       if (strlen(s) > 1) {
          achar = s[1];
          i = 1;
-         while (i < strlen(s)) {
+         while (i < strlen(s) - 1) {
             achar = s[i];
             if (!valid_f(achar))
                valid_id = FALSE;
@@ -22,4 +31,33 @@ int validateIdentifier(char* s) {
       return TRUE;
    else
       return FALSE;
+}
+
+int valid_s(char ch) {
+   if (((ch >= 'A') && (ch <= 'Z'))
+         || ((ch >= 'a') && (ch <= 'z')))
+      return TRUE;
+   else
+      return FALSE;
+}
+
+int valid_f(char ch) {
+   if (((ch >= 'A') && (ch <= 'Z'))
+         || ((ch >= 'a') && (ch <= 'z'))
+         || ((ch >= '0') && (ch <= '9')))
+      return TRUE;
+   else
+      return FALSE;
+}
+
+int main(int argc, char** argv) {
+   if (argc == 1) {
+      printf("Usage: identifier <string>\n");
+   } else {
+      if (validateIdentifier(argv[1])) {
+         printf("Valid\n");
+      } else {
+         printf("Invalid\n");
+      }
+   }
 }
