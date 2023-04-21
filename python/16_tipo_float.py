742526ead41300a284c36a02028ee6052a067761
Matheus Pereira <pereiramatheus064@gmail.com>
2023-04-21 11:59:40 -0300

    tipo float

---
+++ python/16_tipo_float.py
@@ -0,0 +1,26 @@
+"""
+Tipo Float
+
+Tipo real, decimal
+
+Casas decimais
+
+o separador das casas decimais é o ponto '.' e não ','
+
+tupla --> 1, 44
+float --> 1.44
+
+valor1, valor2 = 1,44
+saida --> valor1 = 1 e valor2 = 44
+Chamada de cima é uma dupla atribuição.
+
+Podemos converter um float para um int.
+res = int(valor);
+print(type(res)) ==> <class 'int'> 
+
+Numeros complexos
+variavel = 5j
+type(variavel) --> <class 'complex'>
+
+variavel = -20+6j
+"""
