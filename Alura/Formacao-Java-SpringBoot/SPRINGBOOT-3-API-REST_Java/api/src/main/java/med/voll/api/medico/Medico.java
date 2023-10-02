5e47bec5b2205efd3aa103dfc75d73074b8eb17d
Matheus Pereira <pereiramatheus064@gmail.com>
2023-10-02 10:05:37 -0300

    finalizando o curso

--- Alura/Formacao-Java-SpringBoot/SPRINGBOOT-3-API-REST_Java/api/src/main/java/med/voll/api/medico/Medico.java
+++ Alura/Formacao-Java-SpringBoot/SPRINGBOOT-3-API-REST_Java/api/src/main/java/med/voll/api/medico/Medico.java
@@ -15,6 +15,8 @@ public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private Boolean ativo;
     private String nome;
     private String email;
     private String telefone;
@@ -26,11 +28,31 @@ public class Medico {
     private Endereco endereco;
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(AutalizaCadastroMedico dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
