fdb464309b1d9e1f3028dfc11a4123119a1019c0
Matheus Pereira <pereiramatheus064@gmail.com>
2023-10-05 20:29:52 -0300

    Iniciando curso 3

---
+++ Alura/Formacao-Java-SpringBoot/Curso 3/api/src/main/resources/db/migration/V6__create-table-consultas.sql
@@ -0,0 +1,9 @@
+CREATE TABLE consultas(
+    id BIGINT NOT NULL auto_increment,
+    medico_id BIGINT NOT NULL,
+    paciente_id BIGINT NOT NULL,
+    data DATETIME NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_consultas_medico_id FOREIGN KEY(medico_id) REFERENCES medicos(id),
+    CONSTRAINT fk_consultas_paciente_id FOREIGN KEY(paciente) REFERENCES pacientes(id)
+);
