9cc4551a93dd74745d4135d92f0d70d896ebb580
Matheus Pereira <pereiramatheus064@gmail.com>
2024-11-13 08:50:25 -0300

    starting about waiting

---
+++ java/java-concurrency-notes/src/main/java/DemoBlockedRunnable.java
@@ -0,0 +1,12 @@
+public class DemoBlockedRunnable implements Runnable {
+    @Override
+    public void run() {
+        commonResource();
+    }
+
+    private static synchronized void commonResource() {
+        while (true) {
+            // Simulating heavy process
+        }
+    }
+}
