2a9ae140e1b44c2aa2aa94a0056f92fe395ee854
Matheus Pereira <pereiramatheus064@gmail.com>
2024-12-26 19:48:17 -0300

    finalizando a parte de strings

--- go/bootcamp/unicode-wrapper/main.go
+++ go/bootcamp/unicode-wrapper/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"unicode"
+)
 
+func main() {
+	text := ``
+
+	const maxWidth = 40
+
+	var lw int
+
+	for _, r := range text {
+		fmt.Printf("%c", r)
+
+		switch lw++; {
+		case lw > maxWidth && r != '\n' && unicode.IsSpace(r):
+			fmt.Println()
+			fallthrough
+		case r == '\n':
+			lw = 0
+		}
+	}
 }
