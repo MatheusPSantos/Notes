facf611389a1fa23333b1b1fb8275d98e8d31d23
Matheus Pereira <pereiramatheus064@gmail.com>
2025-04-17 10:48:08 -0300

    iniciando estudos sobre spring batch

---
+++ java/java-spring/spring-batch/springbatchudemy/src/main/java/br/com/matheuspsantos/springbatchudemy/SpringbatchudemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheuspsantos.springbatchudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchudemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbatchudemyApplication.class, args);
+    }
+
+}
