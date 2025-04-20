ef857d87dffb01a845b636fa0f8717ac07145a67
Matheus Pereira <pereiramatheus064@gmail.com>
2025-04-20 07:22:15 -0300

    update project

---
+++ cobol/HELLOWORLD.cob
@@ -0,0 +1,28 @@
+000001*****************************************************************
+000002* Program name:    HELLOWORLD                               
+000003* Original author: MATHEUSPSANTOS                                
+000004*
+000005* Maintenence Log                                              
+      * Date      Author        Maintenance Requirement               
+      * --------- ------------  --------------------------------------- 
+      * 01/01/08 MATHEUSPSANTOS Created for COBOL class         
+      *                                                               
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  HELLOWORLD.
+       AUTHOR. MATHEUSPSANTOS. 
+       INSTALLATION. COBOL DEVELOPMENT CENTER. 
+       DATE-WRITTEN. 01/01/08. 
+       DATE-COMPILED. 01/01/08. 
+       SECURITY. NON-CONFIDENTIAL.
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION. 
+
+       PROCEDURE DIVISION.
+       0100-START-HERE.
+             DISPLAY "Hello World!".
+       STOP RUN.
+       END PROGRAM HELLOWORLD.
+
+       
